2931d3129869d87bb1950665f29ec331b9095328
Vsevolod-P <discaple@gmail.com>
2025-02-10 11:05:21 +0300

    обновил импорт пакетов

--- Diplom_1/src/main/test/java/TestBurger.java
+++ Diplom_1/src/main/test/java/TestBurger.java
@@ -1,8 +1,12 @@
-package praktikum;
+
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
